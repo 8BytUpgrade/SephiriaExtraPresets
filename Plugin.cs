7b0518b96925fe9e477b8a6ead16fff34faf6b37
8BytUpgrade <damianog0001@gmail.com>
2026-08-12 21:39:52 -0400

    init

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace SephiriaPresets;
+
+[BepInPlugin("com.byt.ExtraPresets", "ExtraPresets", "1.0.0")]
+[BepInProcess("Sephiria.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log;
+
+    public void Awake()
+    {
+        // Plugin startup logic
+        Log = Logger;
+        var harmony = new Harmony("com.byt.ExtraPresets");
+        harmony.PatchAll();
+        Logger.LogInfo("Plugin ExtraPresets is loaded!");
+    }
+}
